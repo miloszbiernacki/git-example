05a31058caf718f79b3bc16243dc759ccbdcae06
milosz <milo31@o2.pl>
2019-11-15 15:52:05 +0100

    [change] -- git jest git

--- src/main/java/pl/mpas/git_examples/Hello.java
+++ src/main/java/pl/mpas/git_examples/Hello.java
@@ -3,7 +3,7 @@ package pl.mpas.git_examples;
 public class Hello {
     public static void main(String[] args) {
         System.out.println("hello git");
-
+        System.out.println("git jest git");
         System.out.println("hasta la vista");
 
     }
