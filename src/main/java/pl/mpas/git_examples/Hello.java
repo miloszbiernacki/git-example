2c08b1a287ee230e7081afe1953a3d73b8b91441
milosz <milo31@o2.pl>
2019-11-15 15:51:04 +0100

    [fix] -- pozdrawianie

--- src/main/java/pl/mpas/git_examples/Hello.java
+++ src/main/java/pl/mpas/git_examples/Hello.java
@@ -4,10 +4,7 @@ public class Hello {
     public static void main(String[] args) {
         System.out.println("hello git");
 
-        System.out.println("hello my friend");
         System.out.println("hasta la vista");
-        System.out.println("hello my friend again");
-        System.out.println("quick");
-        System.out.println("fix");
+
     }
 }
